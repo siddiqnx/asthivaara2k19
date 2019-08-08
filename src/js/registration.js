7b7c5a33605c9a6af57fd06a67b026c520a464ee
Siddiq Nx <nxsiddiq@gmail.com>
2019-08-08 19:38:45 +0530

    added automated mail

--- src/js/registration.js
+++ src/js/registration.js
@@ -11,9 +11,6 @@ const openModalButtons = document.querySelectorAll('[data-modal-target]')
 const closeModalButtons = document.querySelectorAll('[data-close-button]')
 const overlay = document.getElementById('overlay');
 
-const sg = require('@sendgrid/mail');
-sg.setApiKey('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');
-
 let registrationType = form.querySelector('#registration-type');
 let type;
 let hasPaid = false;
@@ -104,60 +101,7 @@ form.addEventListener('submit', (e) => {
         hasUploadedScreenshot: (form.user_screenshot.files.length) ? true : false,
       }).then((docRef) => {
         console.log(docRef);
-        var data = {
-          prefix: form.user_prefix.value,
-          name: form.user_name.value,
-          organization: form.user_organization.value,
-          department: form.user_department.value,
-          year: form.user_year.value,
-          address: form.user_address.value,
-          country: form.user_country.value,
-          phoneNumber: form.user_phno.value,
-          email: form.user_email.value,
-          food: form.user_food.value,
-          accommodation: form.user_accommodation.value,
-          paymentDetails: {
-            accountNumber: form.user_accno.value,
-            dateOfTransfer: form.user_dateoftransfer.value,
-            amount: form.user_amount.value,
-            referenceID: form.user_referenceid.value,
-            screenshotURL: url
-          },
-          registrationDate: new Date(),
-          hasUploadedScreenshot: (form.user_screenshot.files.length) ? true : false,
-        };
-        try {
-          const msg = {
-            "personalizations": [
-              {
-                "to": [
-                  {
-                    "email": "nxsiddiq@gmail.com",
-                    "name": "John Doe"
-                  }
-                ],
-                "dynamic_template_data": {
-                  "name": form.user_name.value,
-                  
-                },
-                "subject": "Hello, World!"
-              }
-            ],
-            "from": {
-              "email": "info@asthivaara19.com",
-              "name": "Asthivaara 2K19"
-            },
-            "reply_to": {
-              "email": "noreply@johndoe.com",
-              "name": "John Doe"
-            },
-            "template_id": "d-21f98d2715ee4ab5a84443ec166ba9ec"
-          };
-          sg.send(msg);
-        } catch(err) {
-          console.log(err);
-        }
-        alert(`Registration Successful`);
+        alert(`Registration for Workshop Successful!`);
         form.reset();
         hasPaid = false;
         registerButton.setAttribute('disabled', '');
@@ -186,7 +130,7 @@ form.addEventListener('submit', (e) => {
         registrationDate: new Date(),
         hasUploadedScreenshot: (form.user_screenshot.files.length) ? true : false,
       }).then((docRef) => {
-        alert(`Registration Successful`);
+        alert(`Registration for Symposium Successful!`);
         form.reset();
         hasPaid = false;
         registerButton.setAttribute('disabled', '');
@@ -197,9 +141,9 @@ form.addEventListener('submit', (e) => {
       alert('Choose a valid Registration Type!');
     }
   })
-  // .catch((error) => {
-  //   alert('File Upload Error. Please try again');
-  // });
+  .catch((error) => {
+    alert('File Upload Error. Please try again');
+  });
 });
 
 // Modal Logic
