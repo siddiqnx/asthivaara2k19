41af02479e16ad10c527fae56679763103f1ac42
Siddiq Nx <nxsiddiq@gmail.com>
2019-08-11 04:32:42 +0530

    added flash news

--- src/events_entry.js
+++ src/events_entry.js
@@ -2,7 +2,6 @@ import './css/main.scss';
 import './fonts/IBMPlexSans-Bold.ttf';
 import './fonts/IBMPlexSans-Light.ttf';
 import './js/app';
-import './js/carousel';
 import './js/event-handler';
 import './js/side-nav';
 import './js/animate-on-scroll';
