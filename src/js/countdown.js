4b261edb449c09abf329c52eaa78a6228d0e4614
Siddiq Nx <nxsiddiq@gmail.com>
2019-08-14 17:27:42 +0530

    Event Over

--- src/js/countdown.js
+++ src/js/countdown.js
@@ -1,18 +1,18 @@
-function prezero(n){
-  return n > 9 ? "" + n: "0" + n;
-}
-var deadline = new Date("Aug 14, 2019 08:00:00").getTime(); 
-setInterval(function() { 
-  var now = new Date().getTime(); 
-  var t = deadline - now; 
-  var days = prezero(Math.floor(t / (1000 * 60 * 60 * 24)));
-  var hours = prezero(Math.floor((t % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))); 
-  var minutes = prezero(Math.floor((t % (1000 * 60 * 60)) / (1000 * 60))); 
-  var seconds = prezero(Math.floor((t % (1000 * 60)) / 1000));
-  document.getElementsByClassName("hero__countdown")[0].innerHTML = '<span class="countdown_item"><span class="countdown_num">' + days + '</span><span class="countdown_unit"> Days </span></span>'  
-  + '<span class="countdown_item"><span class="countdown_num">' + hours + '</span><span class="countdown_unit"> Hours </span></span>' + '<span class="countdown_item"><span class="countdown_num">' + minutes + '</span><span class="countdown_unit"> Minutes </span></span>' + '<span class="countdown_item"><span class="countdown_num">' + seconds + '</span><span class="countdown_unit"> Seconds </span></span>'; 
-  if (t < 0) { 
-      clearInterval(x); 
-      document.getElementById("countdown").innerHTML = "Event Started!"; 
-  } 
-}, 1000);
+// function prezero(n){
+//   return n > 9 ? "" + n: "0" + n;
+// }
+// var deadline = new Date("Aug 14, 2019 08:00:00").getTime(); 
+// setInterval(function() { 
+//   var now = new Date().getTime(); 
+//   var t = deadline - now; 
+//   var days = prezero(Math.floor(t / (1000 * 60 * 60 * 24)));
+//   var hours = prezero(Math.floor((t % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))); 
+//   var minutes = prezero(Math.floor((t % (1000 * 60 * 60)) / (1000 * 60))); 
+//   var seconds = prezero(Math.floor((t % (1000 * 60)) / 1000));
+//   document.getElementsByClassName("hero__countdown")[0].innerHTML = '<span class="countdown_item"><span class="countdown_num">' + days + '</span><span class="countdown_unit"> Days </span></span>'  
+//   + '<span class="countdown_item"><span class="countdown_num">' + hours + '</span><span class="countdown_unit"> Hours </span></span>' + '<span class="countdown_item"><span class="countdown_num">' + minutes + '</span><span class="countdown_unit"> Minutes </span></span>' + '<span class="countdown_item"><span class="countdown_num">' + seconds + '</span><span class="countdown_unit"> Seconds </span></span>'; 
+//   if (t < 0) { 
+//       clearInterval(x); 
+//       document.getElementById("countdown").innerHTML = "Event Started!"; 
+//   } 
+// }, 1000);
